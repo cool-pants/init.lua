0ab1e6c354b72af1263f2516c76dde216a82c583
cool-pants <muban.sg@gmail.com>
2024-05-22 11:52:30 +0530

    feat: move luarocks conf to neorg

--- lua/core/lazy/neorg.lua
+++ lua/core/lazy/neorg.lua
@@ -1,8 +1,17 @@
 return {
-    "nvim-neorg/neorg",
-    dependencies = { "luarocks.nvim" },
-    -- put any other flags you wanted to pass to lazy here!
-    config = function()
-        require("neorg").setup()
-    end,
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = function()
+			require("luarocks-nvim").setup({})
+		end,
+	},
+	{
+		"nvim-neorg/neorg",
+		dependencies = { "luarocks.nvim" },
+		-- put any other flags you wanted to pass to lazy here!
+		config = function()
+			require("neorg").setup()
+		end,
+	},
 }
