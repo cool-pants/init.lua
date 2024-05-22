94689b3fa324e4954a47074f0fa0ca1340fb9313
cool-pants <muban.sg@gmail.com>
2024-05-22 11:54:43 +0530

    feat: add oil, flash, nui, noice

---
+++ lua/core/lazy/editor.lua
@@ -0,0 +1,84 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				default_file_explorer = true,
+				columns = {
+					"icon",
+					"permission",
+					"size",
+				},
+				preview = {
+					-- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+					-- min_width and max_width can be a single value or a list of mixed integer/float types.
+					-- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
+					max_width = 0.9,
+					-- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
+					min_width = { 40, 0.4 },
+					-- optionally define an integer/float for the exact width of the preview window
+					width = nil,
+					-- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+					-- min_height and max_height can be a single value or a list of mixed integer/float types.
+					-- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
+					max_height = 0.9,
+					-- min_height = {5, 0.1} means "the greater of 5 columns or 10% of total"
+					min_height = { 5, 0.1 },
+					-- optionally define an integer/float for the exact height of the preview window
+					height = nil,
+					border = "rounded",
+					win_options = {
+						winblend = 0,
+					},
+					-- Whether the preview window is automatically updated when the cursor is moved
+					update_on_cursor_moved = true,
+				},
+			})
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		end,
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		optional = true,
+		opts = function(_, opts)
+			local function flash(prompt_bufnr)
+				require("flash").jump({
+					pattern = "^",
+					label = { after = { 0, 0 } },
+					search = {
+						mode = "search",
+						exclude = {
+							function(win)
+								return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "TelescopeResults"
+							end,
+						},
+					},
+					action = function(match)
+						local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+						picker:set_selection(match.pos[1] - 1)
+					end,
+				})
+			end
+			opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+				mappings = { n = { s = flash }, i = { ["<c-s>"] = flash } },
+			})
+		end,
+	},
+}
---
+++ lua/core/lazy/ui.lua
@@ -0,0 +1,42 @@
+return {
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
+	{
+		"rcarriga/nvim-notify",
+		keys = {
+			{
+				"<leader>un",
+				function()
+					require("notify").dismiss({ silent = true, pending = true })
+				end,
+				desc = "Dismiss All Notifications",
+			},
+		},
+		opts = {
+			stages = "static",
+			timeout = 3000,
+			max_height = function()
+				return math.floor(vim.o.lines * 0.75)
+			end,
+			max_width = function()
+				return math.floor(vim.o.columns * 0.75)
+			end,
+			on_open = function(win)
+				vim.api.nvim_win_set_config(win, { zindex = 100 })
+			end,
+		},
+	},
+}
