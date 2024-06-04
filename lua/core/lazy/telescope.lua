9bb32c183c323cfd50185d9d83b48bcd4e0a9bd4
cool-pants <muban.sg@gmail.com>
2024-06-04 14:20:17 +0530

    feat: update telescope conf

--- lua/core/lazy/telescope.lua
+++ lua/core/lazy/telescope.lua
@@ -38,8 +38,8 @@ return {
 				file_ignore_patterns = {
 					"node_modules",
 					"venv",
-					".env",
 					".venv",
+					".git/",
 					"__pycache__",
 				},
 			},
@@ -55,7 +55,9 @@ return {
 		pcall(require("telescope").load_extension, "ui-select")
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>pf", function()
+			builtin.find_files({ hidden = true })
+		end, {})
 		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 		vim.keymap.set("n", "<leader>pws", function()
 			local word = vim.fn.expand("<cword>")
