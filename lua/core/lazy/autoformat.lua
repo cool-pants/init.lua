4cab81e2012a6684f91b1b81d4b57e367801c76c
cool-pants <muban.sg@gmail.com>
2024-06-07 14:52:12 +0530

    feat(conform): add formatter for typescript

--- lua/core/lazy/autoformat.lua
+++ lua/core/lazy/autoformat.lua
@@ -23,12 +23,47 @@ return { -- Autoformat
 				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
 			}
 		end,
+		formatters = {
+			prettier = {
+				command = "prettier",
+				options = {
+					-- Use a specific prettier parser for a filetype
+					-- Otherwise, prettier will try to infer the parser from the file name
+					ft_parsers = {
+						javascript = "babel",
+						javascriptreact = "babel",
+						typescript = "typescript",
+						typescriptreact = "typescript",
+						vue = "vue",
+						css = "css",
+						scss = "scss",
+						less = "less",
+						html = "html",
+						json = "json",
+						jsonc = "json",
+						yaml = "yaml",
+						markdown = "markdown",
+						["markdown.mdx"] = "mdx",
+						graphql = "graphql",
+						handlebars = "glimmer",
+					},
+					-- Use a specific prettier parser for a file extension
+					ext_parsers = {
+						qmd = "markdown",
+					},
+				},
+			},
+		},
 		formatters_by_ft = {
 			lua = { "stylua" },
 			-- Conform can also run multiple formatters sequentially
 			python = { "isort", "black" },
 			golang = { "gofumpt" },
 			javascript = { { "prettierd", "prettier" } },
+			javascriptreact = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
+			typescriptreact = { { "prettierd", "prettier" } },
+			json = { { "prettierd", "prettier" } },
 			--
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
