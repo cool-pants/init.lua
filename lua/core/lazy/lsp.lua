e351bd5fbabf9d8ca5f28f9fd10bc9e6928019ac
cool-pants <muban.sg@gmail.com>
2024-06-07 10:31:09 +0530

    feat: add tsserver lsp

--- lua/core/lazy/lsp.lua
+++ lua/core/lazy/lsp.lua
@@ -186,6 +186,27 @@ return { -- LSP Configuration & Plugins
 				root_dir = require("lspconfig").util.root_pattern("Dockerfile"),
 				single_file_support = true,
 			},
+			tsserver = {
+				cmd = { "typescript-language-server", "--stdio" },
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"javascript.jsx",
+					"typescript",
+					"typescriptreact",
+					"typescript.tsx",
+				},
+				init_options = {
+					hostInfo = "neovim",
+				},
+				root_dir = require("lspconfig").util.root_pattern(
+					"tsconfig.json",
+					"jsconfig.json",
+					"package.json",
+					".git"
+				),
+				single_file_support = true,
+			},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
 			-- Some languages (like typescript) have entire language plugins that can be useful:
