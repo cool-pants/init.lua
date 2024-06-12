de69f33123655578576255e0337f11bc9717d65c
cool-pants <muban.sg@gmail.com>
2024-06-12 17:55:14 +0530

    feat(mini): use mini.hipatterns for hex color

--- lua/core/lazy/mini.lua
+++ lua/core/lazy/mini.lua
@@ -3,6 +3,7 @@ return { -- Collection of various small independent plugins/modules
 	config = function()
 		-- Better Around/Inside textobjects
 		--
+		-- #000000
 		-- Examples:
 		--  - va)  - [V]isually select [A]round [)]paren
 		--  - yinq - [Y]ank [I]nside [N]ext [']quote
@@ -11,10 +12,24 @@ return { -- Collection of various small independent plugins/modules
 		require("mini.ai").setup({ n_lines = 500 })
 		-- Add/delete/replace surroundings (brackets, quotes, etc.)
 		--
+		-- #00ff00
 		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
 		-- - sd'   - [S]urround [D]elete [']quotes
 		-- - sr)'  - [S]urround [R]eplace [)] [']
 		require("mini.surround").setup()
+		local hipatterns = require("mini.hipatterns")
+		hipatterns.setup({
+			highlighters = {
+				-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+				fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+				hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+				todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+				note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+				-- Highlight hex color strings (`#rrggbb`) using that color
+				hex_color = hipatterns.gen_highlighter.hex_color(),
+			},
+		})
 
 		-- Simple and easy statusline.
 		--  You could remove this setup call if you don't like it,
