a1d48583bbb99a35732853bd8d777b4a1badbc86
cool-pants <muban.sg@gmail.com>
2024-06-20 12:40:25 +0530

    feat(oil): add hidden files based on gitignore

--- lua/core/lazy/editor.lua
+++ lua/core/lazy/editor.lua
@@ -1,3 +1,23 @@
+local git_ignored = setmetatable({}, {
+	__index = function(self, key)
+		local proc = vim.system({ "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" }, {
+			cwd = key,
+			text = true,
+		})
+		local result = proc:wait()
+		local ret = {}
+		if result.code == 0 then
+			for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+				-- Remove trailing slash
+				line = line:gsub("/$", "")
+				table.insert(ret, line)
+			end
+		end
+
+		rawset(self, key, ret)
+		return ret
+	end,
+})
 return {
 	{
 		"numToStr/Comment.nvim",
@@ -5,7 +25,19 @@ return {
 	},
 	{
 		"stevearc/oil.nvim",
-		opts = {},
+		opts = {
+			view_options = {
+				is_hidden_file = function(name, _)
+					local dir = require("oil").get_current_dir()
+					-- if no local directory (e.g. for ssh connections), always show
+					if not dir then
+						return false
+					end
+					-- Check if file is gitignored
+					return vim.list_contains(git_ignored[dir], name)
+				end,
+			},
+		},
 		-- Optional dependencies
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
