055d25457957615c98fd08b32c16c3e80598378a
cool-pants <muban.sg@gmail.com>
2024-06-20 12:39:57 +0530

    feat(lang): add eslint and tailwind conf

--- lua/core/languages/init.lua
+++ lua/core/languages/init.lua
@@ -11,6 +11,8 @@ M.get_languages = function()
 		"docker",
 		"css",
 		"css_vars",
+		"ecmascript",
+		"tailwindcss",
 	}
 end
 
@@ -19,11 +21,16 @@ M.get_formatters = function()
 	local formatters = {}
 	for _, value in ipairs(M.get_languages()) do
 		local lang = require("core.languages." .. value)
+		if lang.conf ~= nil then
+			formatters[value] = lang.conf
+			goto continue
+		end
 		if lang.formatters ~= nil then
 			for _, fmt in ipairs(lang.format_files) do
 				formatters[fmt] = { lang.formatters }
 			end
 		end
+		::continue::
 	end
 	return formatters
 end
