fe38c3696a223b1776603ef58c8b9f6c74607920
cool-pants <muban.sg@gmail.com>
2024-07-03 21:31:25 +0530

    feat(tools): tweak gitsigns

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -3,11 +3,20 @@ local config = {}
 function config.gitsigns()
 	require("gitsigns").setup({
 		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
+			add = { text = "┃" },
+			change = { text = "┃" },
 			delete = { text = "_" },
 			topdelete = { text = "‾" },
 			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+		signs_staged = {
+			add = { text = "┃" },
+			change = { text = "┃" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
 		},
 	})
 end
