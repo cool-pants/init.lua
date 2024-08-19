9eb5df7c774e6634c80d2ccb3859a3aa011a1147
cool-pants <muban.sg@gmail.com>
2024-08-19 14:19:07 +0530

    fix(lsp): fix lspconfig capabilities

--- lua/pants/configs/lspconfig/utils.lua
+++ lua/pants/configs/lspconfig/utils.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
--- M.capabilities = vim.tbl_deep_extend("force", M.capabilities, require("cmp_nvim_lsp").default_capabilities())
+M.capabilities = vim.tbl_deep_extend("force", M.capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 function M._attach(client, _)
 	vim.opt.omnifunc = "v:lua.vim.lsp.omnifunc"
